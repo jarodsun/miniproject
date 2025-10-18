b665031a28cce70ea5b7e2037724e79f8e1a928b
Jarod Sun <garsday@gmail.com>
2025-10-18 13:45:33 +0800

    更换初始化项目，从js版本，更改为ts版本。

--- drone-pilot/wx/miniprogram/pages/index/index.ts
+++ drone-pilot/wx/miniprogram/pages/index/index.ts
@@ -1,4 +1,6 @@
-// index.js
+// index.ts
+// 获取应用实例
+const app = getApp<IAppOption>()
 const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
 
 Component({
@@ -16,10 +18,10 @@ Component({
     // 事件处理函数
     bindViewTap() {
       wx.navigateTo({
-        url: '../logs/logs'
+        url: '../logs/logs',
       })
     },
-    onChooseAvatar(e) {
+    onChooseAvatar(e: any) {
       const { avatarUrl } = e.detail
       const { nickName } = this.data.userInfo
       this.setData({
@@ -27,7 +29,7 @@ Component({
         hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
       })
     },
-    onInputChange(e) {
+    onInputChange(e: any) {
       const nickName = e.detail.value
       const { avatarUrl } = this.data.userInfo
       this.setData({
@@ -35,7 +37,7 @@ Component({
         hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
       })
     },
-    getUserProfile(e) {
+    getUserProfile() {
       // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
       wx.getUserProfile({
         desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
